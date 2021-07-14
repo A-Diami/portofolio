a242032bed6cd695eeae8591c4b3ba914eee3ed1
diawa12 <diawa727@gmail.com>
2021-07-14 12:43:55 +0000

    first commit

---
+++ front/main.js
@@ -0,0 +1,19 @@
+var type = new Typed(".profil", {
+    strings: ["je suis Diallo Adama Hawa",
+                " Développeuse Full Stack Junior"
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+});
+
+var type2 = new Typed(".metier", {
+    strings: [
+                " Développeuse Full Stack Junior"
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+})
+
+
